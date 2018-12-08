d329812cb3b33ab7ac2c72502da1a8cd46a1ff61
ikasamt <ikasamt@gmail.com>
2018-12-08 19:29:08 +0900

    add uniqint

--- zapp/uniq.go
+++ zapp/uniq.go
@@ -11,3 +11,15 @@ func UniqString(src []string) []string {
 	}
 	return ret
 }
+
+func UniqInt(src []int) []int {
+	ret := make([]int, 0, len(src))
+	srcMap := make(map[int]struct{}, len(src))
+	for _, n := range src {
+		if _, ok := srcMap[n]; !ok {
+			srcMap[n] = struct{}{}
+			ret = append(ret, n)
+		}
+	}
+	return ret
+}
